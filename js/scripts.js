2311b7e2199464bb6d069d3890024871c0750cc5
Abeer Khakwani & kenna warsinske <student@epicodus.com>
2015-04-08 11:25:49 -0700

    add logic for plural and dollar values

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,98 @@
 var coinCombination = function(input){
 
     var returnArray=[];
-    input =parseInt(input);
+    //input= input.toFixed(2);
+
+
+    input = input * 100;
+
+    if ((input != 0)&& (input > 999)){
+        var tenDollar = Math.floor(input/1000);
+        if(tenDollar > 1){
+            returnArray.push(tenDollar + " Tens");
+        }
+        else{
+            returnArray.push(tenDollar + " Ten");
+        }
+
+        input = input % 1000;
+        console.log(input);
+    }
+
+    if ((input != 0)&& (input > 499)){
+        var fiveDollar = Math.floor(input/500);
+        if(fiveDollar > 1){
+            returnArray.push(fiveDollar + " Fives");
+        }
+        else{
+            returnArray.push(fiveDollar + " Five");
+        }
+
+        input = input % 500;
+        console.log(input);
+    }
+
+    if ((input != 0)&& (input > 99)){
+        var dollar = Math.floor(input/100);
+        if(dollar > 1){
+            returnArray.push(dollar + " Dollars");
+        }
+        else{
+            returnArray.push(dollar + " Dollar");
+        }
+
+        input = input % 100;
+        console.log(input);
+    }
 
     if ((input != 0)&& (input > 24)){
-        var quarter = parseInt(input/25);
-        returnArray.push(quarter + " Quarter");
+        var quarter = Math.floor(input/25);
+        if(quarter > 1){
+            returnArray.push(quarter + " Quarters");
+        }
+        else{
+            returnArray.push(quarter + " Quarter");
+        }
+
         input = input % 25;
-        console.log(input);
+
     }
 
     if ((input != 0)&& (input > 9)) {
-        var dime = parseInt(input/10);
-        returnArray.push(dime + " Dime");
-        input = input % 10;
         console.log(input);
+        var dime = Math.floor(input/10);
+        if(dime > 1){
+            returnArray.push(dime + " Dimes");
+        }
+        else{
+            returnArray.push(dime + " Dime");
+        }
+
+
+        input = input % 10;
+
     }
 
     if ((input != 0)&&(input > 4)){
-        var nickel = parseInt(input/5);
-        returnArray.push(nickel + " Nickel");
+        var nickel = Math.floor(input/5);
+        if(nickel > 1){
+            returnArray.push(nickel + " Nickels");
+        }
+        else{
+            returnArray.push(nickel + " Nickel");
+        }
         input = input % 5;
-        console.log(input);
-    }
 
-    if (input != 0){
-        returnArray.push(input + " Penny");
+    }
+    input = Math.floor(input);
+    if ((input != 0)&&(input < 5)){
+        if(input > 1){
+            returnArray.push(input + " Pennies");
+        }
+        else{
+            returnArray.push(input + " Penny");
+        }
+
     }
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -6,26 +6,46 @@ describe('coinCombination', function() {
     //     expect(CoinCombination(5)).to.equal("nickel");
     // });
     it("Takes in value of 50 and return two quarters", function(){
-        expect(coinCombination(50)).to.equal("2 Quarter");
+        expect(coinCombination(.50)).to.equal("2 Quarters");
     });
 
-    it("Takes in value of 55 and return two quarters", function(){
-        expect(coinCombination(55)).to.equal("2 Quarter 1 Nickel");
+    it("Takes in value of 55 and return two quarters 1 nickel", function(){
+        expect(coinCombination(.55)).to.equal("2 Quarters 1 Nickel");
     });
 
-    it("Takes in value of 60 and return two quarters", function(){
-        expect(coinCombination(60)).to.equal("2 Quarter 1 Dime");
+    it("Takes in value of 60 and return two quarters 1 dime", function(){
+        expect(coinCombination(.60)).to.equal("2 Quarters 1 Dime");
     });
 
-    it("Takes in value of 68 and return two quarters", function(){
-        expect(coinCombination(68)).to.equal("2 Quarter 1 Dime 1 Nickel 3 Penny");
+    it("Takes in value of 68 and returns 2 Quarter 1 Dime 1 Nickel 3 Pennys", function(){
+        expect(coinCombination(.68)).to.equal("2 Quarters 1 Dime 1 Nickel 3 Pennies");
     });
 
-    it("Takes in value of 128 and return two quarters", function(){
-        expect(coinCombination(128)).to.equal("5 Quarter 3 Penny");
+    it("Takes in value of 128 and return  5 quarters 3 pennies", function(){
+        expect(coinCombination(1.28)).to.equal("1 Dollar 1 Quarter 3 Pennies");
     });
-    it("Takes in value of 4 and returns 4 pennies", function(){
-        expect(coinCombination(4)).to.equal("4 Penny");
+    it("Takes in value of 4.53 and returns 18 Quarter 3 Penny", function(){
+        expect(coinCombination(4.53)).to.equal("4 Dollars 2 Quarters 3 Pennies");
+    });
+
+    it("Takes in value of 5 and return one nickel", function(){
+        expect(coinCombination(.05)).to.equal("1 Nickel");
+    });
+
+    it("Takes in value of 10 and return one dime", function(){
+        expect(coinCombination(.10)).to.equal("1 Dime");
+    });
+
+    it("Takes in value of 30 and return one Quarter 1 nickel", function(){
+        expect(coinCombination(.30)).to.equal("1 Quarter 1 Nickel");
+    });
+
+    it("Takes in value of 565 and return one Quarter nickel", function(){
+        expect(coinCombination(5.65)).to.equal("1 Five 2 Quarters 1 Dime 1 Nickel");
+    });
+
+    it("Takes in value of 15.65 and return one Quarter nickel", function(){
+        expect(coinCombination(30.65)).to.equal("3 Tens 2 Quarters 1 Dime 1 Nickel");
     });
 
 
